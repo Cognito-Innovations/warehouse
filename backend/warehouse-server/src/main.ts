138cae367ce50478c8b87e7e3de597745df9acf8
saurabh-pingale <saurabh.pingale@formpilot.org>
2025-11-07 23:44:06 +0530

    Added elastic beanstalk and sh files to work with the AWS beanstalk

--- backend/warehouse-server/src/main.ts
+++ backend/warehouse-server/src/main.ts
@@ -44,7 +44,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.PORT || 3001;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server running on port ${port}`);
 }
 
 bootstrap().catch((err) => {
