8221291a9cfc5f73c6fa1e4819d8164689a4149b
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2025-11-10 22:43:37 +0530

    removed ignore and platform hooks file

--- backend/warehouse-server/.platform/hooks/prebuild/prevent-npm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Prebuild: Creating empty node_modules to skip npm install" >> /var/log/eb-hooks.log
-mkdir -p node_modules
-echo "Prebuild done: node_modules created (empty)" >> /var/log/eb-hooks.log
